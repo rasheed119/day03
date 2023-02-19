bc5bed1988820f7a7e30ff770b8e322501886f86
rasheed119 <78138853+rasheed119@users.noreply.github.com>
2023-02-19 15:15:12 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,59 @@
+var resume  = [{
+    "Name" : "M.Abdur Rasheed",
+    "email" : "abd205200@gmail.com",
+    "Contect number" : "+91 7598872817",
+    "Linkedin" : "https://www.linkedin.com/in/abdur-rasheed-0813201aa/",
+},
+{
+    "Education":[{
+        "Qualification": "B.E",
+        "Institution" : "Francis Xavier engineering College",
+        "Year of passing" : "2021",
+        "CGPA": "7.6"
+    },
+    {
+        "Qualification" : "HSC",
+        "Institution" : "Rosemary matric higher secondary school",
+        "Year of passing": "2017",
+        "Percentage" : "87%"
+    },
+    {
+        "Qualification" : "SSLC",
+        "Institution" : "Little flower matric higher secondary school",
+        "Year of passing": "2015",
+        "Percentage" : "89%"
+    }]
+},{
+    "Experience" : [{
+        "Title" : "Machine operator",
+        "Company" : "ZF Wabco india limited",
+        "Location" : "Mahindra world city",
+        "Start Date" : "June 2021",
+        "End Date" : "December 2021"
+    }]
+},{
+    "Skills" : 
+    [
+        {
+            "Programing language" : "Python",
+            "level" : "Beginner" 
+        },
+        {
+            "Programing language" : "Java",
+            "level" : "Beginner"
+        }
+    ]
+},{
+    "Project name" : " Investigation of mechanical properties of tig welded 5A06 aluminum alloy and sus321 austenitic stainless steel"
+},{
+    "Certification" : 
+    [
+        {
+            "Certificate_1" : "Done implant training in sankar cements Pvt Ltd and unique technique"
+        },{
+            "Certificate_2" : "Volunteer in ISRO expo conducted in Francis Xavier engineering college"
+        },{
+            "Certificate_3" : "Winter skill training development programme on “Design,Fabrication and demonstration of bike/kart in francis Xavier engineering college"
+        }]
+}]
+console.log(resume[0].email)
